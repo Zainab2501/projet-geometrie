fa4fd3d9320c99bd9114f675f86360c5f46286cb
Zainab2501 <zainabelhaissouni@gmail.com>
2026-05-25 16:11:34 +0100

    Carre.java

---
+++ Carre.java
@@ -0,0 +1,10 @@
+package geometrie.formes;
+
+import geometrie.base.Point;
+
+public class Carre extends Rectangle {
+
+    public Carre(Point ancre, double cote) {
+        super(ancre, cote, cote);
+    }
+}
